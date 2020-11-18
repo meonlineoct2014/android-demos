ee70f61cbd2f86ead371804db2d736a8a4abc135
Ashutosh Deshmukh <ashutosh.deshmukh2008@gmail.com>
2020-11-18 20:44:49 +0530

    recyclerview with views added on the fly

---
+++ app/src/main/java/com/android/androidappdemos/interfaces/ItemRequestInterface.kt
@@ -0,0 +1,5 @@
+package com.android.androidappdemos.interfaces
+
+interface ItemRequestInterface {
+    fun addFoodItem()
+}
---
+++ app/src/main/java/com/android/androidappdemos/models/ItemRequestRow.kt
@@ -0,0 +1,9 @@
+package com.android.androidappdemos.models
+
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Spinner
+
+class ItemRequestRow(val imageRadioButton: Int, val fruitName: EditText, val quanitySelecter: Spinner) {
+
+}
