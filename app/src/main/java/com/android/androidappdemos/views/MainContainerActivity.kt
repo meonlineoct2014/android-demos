9a305b82bc23f219ff04a984dcd0d52f43d0605a
Ashutosh Deshmukh <ashutosh.deshmukh2008@gmail.com>
2020-12-24 15:59:13 +0530

    added strictmode code

--- app/src/main/java/com/android/androidappdemos/views/MainContainerActivity.kt
+++ app/src/main/java/com/android/androidappdemos/views/MainContainerActivity.kt
@@ -2,11 +2,25 @@ package com.android.androidappdemos.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import com.android.androidappdemos.BuildConfig
 import com.android.androidappdemos.R
 
 class MainContainerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_container)
+        enableStrictMode()
+    }
+
+    private fun enableStrictMode() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build()
+            )
+        }
     }
 }
